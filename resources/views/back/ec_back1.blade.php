9accd140bff136cf1ec45adbe292afaa5d6bf46c
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-12-17 04:03:20 +0000

    フロント画面作成中

--- resources/views/back/ec_back1.blade.php
+++ resources/views/back/ec_back1.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.ec_base_back1')
 
 @section('content')
-
-    <div>
-        <h2>ECバック画面</h2>
-    </div>
-    
+    <header>
+        <div class="container">
+            <div class="header-title-area">
+                <h1 class="logo">ECバック画面</h1>
+                <p class="text-sub">迅速なお客様対応を心がけましょう</p>
+            </div>
+        </div>
+    </header>
 @endsection
--- resources/views/front/ec_front1.blade.php
+++ resources/views/front/ec_front1.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.ec_base_front1')
 
 @section('content')
+    <header>
+        <div class="container">
+            <div class="header-title-area">
+                <h1 class="logo">ECフロント画面</h1>
+                <p class="text-sub">便利な商品を提供します。</p>
+            </div>
+            <ul class="header-navigation">
+                <li><a href="#">タイヤ</a></li>
+                <li><a href="#">オイル</a></li>
+                <li><a href="#">バッテリー</a></li>
+                <li><a href="#">車検</a></li>
+            </ul>
+        </div>
+    </header>
 
-    <div>
-        <h2>ECフロント画面</h2>
-    </div>
     
 @endsection
