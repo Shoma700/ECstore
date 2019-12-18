a6000690c4377640050ef91b21d608e03c593f29
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-12-18 12:48:34 +0000

    商品グループの絞り込みができた

--- app/Http/Controllers/ECfrontController.php
+++ app/Http/Controllers/ECfrontController.php
@@ -9,20 +9,22 @@ use App\Product;
 
 class ECfrontController extends Controller
 {
-    public function front1_()
-    {
-        return view('front.ec_front1');
-    }
+    // public function front1()
+    // {
+    //     return view('front.ec_front1');
+    // }
+    
     public function front1(Request $request)
     {
-        // $search_product_class = $request->search_product_class;
-        // if ($search_product_class != '') {
-        //     $posts = Product::where('product_class', $search_product_class)->get();
-        // } else {
-        //     $posts = Product::all();
-        // }
-        // return view('front.ec_front1', ['posts' => $posts, 'search_product_class' => $search_product_class]);
-        $posts = Product::all();
+        $search_product_class = $request->search_product_class;
+        // dd($search_product_class);
+        if ($search_product_class != '') {
+            $posts = Product::where('product_class', $search_product_class)->get();
+        } else {
+            $posts = Product::all();
+        }
         return view('front.ec_front1', ['posts' => $posts]);
+        // $posts = Product::all();
+        // return view('front.ec_front1', ['posts' => $posts]);
     }
 }
--- resources/views/front/ec_front1.blade.php
+++ resources/views/front/ec_front1.blade.php
@@ -7,15 +7,18 @@
                 <h1 class="logo">ECフロント画面</h1>
                 <p class="text-sub">スケートボード商品を提供します。</p>
             </div>
+            {{-- 商品分類で絞りこみ --}}
             <ul class="header-navigation">
-                <li><a href="#">デッキ</a></li>
-                <li><a href="#">トラック</a></li>
-                <li><a href="#">ウィール</a></li>
-                <li><a href="#">ベアリング</a></li>
-                <li><a href="#">メンテナンス</a></li>
-                <li><a href="#">シューズ</a></li>
-                <li><a href="#">その他</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => '']) }}">全て</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'デッキ']) }}">デッキ</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'トラック']) }}">トラック</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'ウィール']) }}">ウィール</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'ベアリング']) }}">ベアリング</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'メンテナンス']) }}">メンテナンス</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'シューズ']) }}">シューズ</a></li>
+                <li class="hover"><a href="{{ action('ECfrontController@front1', ['search_product_class' => 'その他']) }}">その他</a></li>
             </ul>
+            {{-- ↑ ↑ ↑ --}}
         </div>
     </header>
     <div class="main">
@@ -29,7 +32,7 @@
                     <div class="product-list-area">
                         @foreach($posts as $p_list)
                         <div class="product-list">
-                            <img src="{{ asset('/storage/app/public/b_ninja.jpg') }}" class="product-image">
+                            <img src="{{ asset('storage/image/' . $p_list->product_image_path) }}" class="product-image">
                             <p class="product-name">{{ mb_substr($p_list->product_name, 0, 12) }}</p>
                             <p class="product-price">{{ $p_list->product_price }}￥</p>
                             <p><input class="num_text" type="text" name="quantity" rows="1">--数量</p>
@@ -42,7 +45,7 @@
                 </div>
                 {{--  --}}
                 <div class="card-contents">
-                    <h4 class="text-title">ページネーション</h4></h4>
+                    <h4 class="text-title2">ページネーション</h4></h4>
                 </div>
             </div>
             {{-- 右側 --}}
@@ -52,15 +55,20 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="under-contents">
+                <div class="card-contents">
+                    <h2 class="text-title">Information</h2>
+                    <ul class="information-list">
+                        <li>2019/12/17 EC店をオープンしました。</li>
+                        <li>2019/12/20 新商品を追加しました。</li>
+                        <li>2020/01/20 イベントの予定です。</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-contents">
-        <h2 class="text-title">Information</h2>
-        <ul class="information-list">
-            <li>2019/12/17 EC店をオープンしました。</li>
-            <li>2019/12/20 新商品を追加しました。</li>
-            <li>2020/01/20 イベントの予定です。</li>
-        </ul>
-    </div>
+
     
     
 @endsection
