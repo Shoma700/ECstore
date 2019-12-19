97e7dc417a40e7e82f391c15c177435b7480b477
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2019-12-19 02:14:37 +0000

    ひとまずカートにいれたい

--- app/Http/Controllers/ECfrontController.php
+++ app/Http/Controllers/ECfrontController.php
@@ -16,6 +16,7 @@ class ECfrontController extends Controller
     
     public function front1(Request $request)
     {
+        //カテゴリの絞り込み
         $search_product_class = $request->search_product_class;
         // dd($search_product_class);
         if ($search_product_class != '') {
@@ -23,6 +24,17 @@ class ECfrontController extends Controller
         } else {
             $posts = Product::all();
         }
+        
+        
+        //カートへの移動
+        // $stock_product_cd = $request->stock_product_cd;
+        // //dd($stock_product_cd);
+        // $cart = Cart::find($$request->stock_product_cd);
+        // //dd($news);
+        // if (empty($news)) {
+        // abort(404);    
+        // }
+        
         return view('front.ec_front1', ['posts' => $posts]);
         // $posts = Product::all();
         // return view('front.ec_front1', ['posts' => $posts]);
