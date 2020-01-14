fda556eaaea91ae50a21a906ff798e7d7cc99a37
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2020-01-14 03:16:51 +0000

    共通関数かできない…

--- app/Http/Controllers/ECfrontController.php
+++ app/Http/Controllers/ECfrontController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Product;
 use App\Cart;
+//↓共通関数はこちら
+use App\libs\Common_Function;
 
 class ECfrontController extends Controller
 {
@@ -16,27 +18,24 @@ class ECfrontController extends Controller
     
     public function front1(Request $request)
     {
-        //カテゴリの絞り込み
-        $search_product_class = $request->search_product_class;
-        // dd($search_product_class);
-        if ($search_product_class != '') {
-            $posts = Product::where('product_class', $search_product_class)->paginate(5);
-        } else {
-            //$posts = Product::all();
-            $posts = Product::paginate(5);
-        }
+        Common_Function::group($request);
+        
+        // //カテゴリの絞り込み(同一のものを共通関数側にも格納)
+        // $search_product_class = $request->search_product_class;
+        // // dd($search_product_class);
+        // if ($search_product_class != '') {
+        //     $posts = Product::where('product_class', $search_product_class)->paginate(5);
+        // } else {
+        //     //$posts = Product::all();
+        //     $posts = Product::paginate(5);
+        // }
         return view('front.ec_front1', ['posts' => $posts, 'search_product_class' => $search_product_class]);
     }    
         
     public function front2(Request $request)
     {
         
-        if ($search_product_class != '') {
-            $posts = Product::where('product_class', $search_product_class)->paginate(5);
-        } else {
-            //$posts = Product::all();
-            $posts = Product::paginate(5);
-        }
+        Common_Function::group($request);
         //カートへの移動
         //検索された商品コードの商品情報をproductsテーブルから取得する
         //$request->session()->put('search_product_cd',1);
---
+++ app/libs/Common_Function.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\libs;
+use Illuminate\Http\Request;
+use App\Product;
+use App\Cart;
+Class Common_Function
+{
+    public static function group($request)
+    {
+        //カテゴリの絞り込み(同一のものを共通関数側にも格納)
+        $search_product_class = $request->search_product_class;
+        // dd($search_product_class);
+        if ($search_product_class != '') {
+            $posts = Product::where('product_class', $search_product_class)->paginate(5);
+            return (['posts' => $posts, 'search_product_class' => $search_product_class]);
+        } else {
+            //$posts = Product::all();
+            $posts = Product::paginate(5);
+            return (['posts' => $posts, 'search_product_class' => $search_product_class]);
+        }
+    }
+    
+}
