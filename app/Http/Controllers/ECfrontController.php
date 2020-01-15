a546ba0545747d45b982245c8d6285c0e6436a88
Shoma700 <ec2-user@ip-172-31-27-225.us-east-2.compute.internal>
2020-01-15 12:36:55 +0000

    カートできそう”

--- app/Http/Controllers/ECfrontController.php
+++ app/Http/Controllers/ECfrontController.php
@@ -36,13 +36,43 @@ class ECfrontController extends Controller
     public function front2(Request $request)
     {
         //カートへの移動(session)
-        //$request->session()->put('search_product_cd',1);
-        //session(['search_product_cd' => 111, 'search_product_quantity' => 222]);
-        $search_product_cd = $request->search_product_cd;
-        $search_product_quantity = $request->search_product_quantity;
-        session([$search_product_cd => $search_product_quantity]);
-        //dump(session());
-        ★if (session)
+        
+        //フォームからpostされた商品情報
+        $prefix = 'product_id-';
+        $search_product_cd = $prefix . $request->search_product_cd;//商品CD
+            dump("postされた商品CD: " . $search_product_cd);//★
+        $search_product_quantity = intval($request->search_product_quantity);//商品数量
+            dump("postされた商品数量: " .$search_product_quantity);//★
+        //sessionに商品が含まれるかチェック
+        if($request->session()->exists($search_product_cd))
+        {
+            dump("↑この商品はすでにsession内にあったよ！");//★
+            //既にsession内に商品CDがある場合
+            //合致するキーの値を取り出し、数量を合算する
+            $value = intval($request->session()->get($search_product_cd));
+            $value_plus = intval($value + $search_product_quantity);
+            dump("ちなみに数量は: " . $value . "あり");//★
+            dump("今回の数量: " . $search_product_quantity . "と合わせ");//★
+            dump("合計: " . $value_plus . "をsessionに格納する");//★
+                // 合致するキーのデータを一旦セッションから削除する
+                $request->session()->forget($search_product_cd);
+                //sessionに改めてキーと合算数量を追加する
+                $request->session()->put('cart.' . $search_product_cd, $value_plus);            
+        }else{
+            //sessionに追加する
+            dump("↑この商品はsession内に無かったので、新規追加するよ！");//★
+            
+            $request->session()->put('cart.' . $search_product_cd, $search_product_quantity);
+        }
+
+        //die;
+        //$request->session()->flush();
+        dump(session()->get('cart'));//★
+        
+        //cartへ商品明細と合計金額を表示させる
+        
+        
+        
         
         //↓同Controller内の関数を使用する(self::関数名(引数))
         //↓関数から戻り値を受ける場合は変数に格納する
